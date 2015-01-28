349836f0c4bebdd3f3a63db5fde09202ecac0921
Ningrui Li <ningrui.li@duke.edu>
2015-01-28 16:29:00 -0500

    added code for generating plot of signal in time domain

--- matlab_to_python/neuroscouting_exercise_python.py
+++ matlab_to_python/neuroscouting_exercise_python.py
@@ -1,5 +1,5 @@
-from numpy import *
-import scipy
+from scipy import *
+import matplotlib.pyplot as plt
 
 sampling_rate = 500.0
 Filt = array([58, 82])
@@ -10,3 +10,12 @@ freq2 = 35
 freq3 = 10
 
 signal = (1./3)*(sin(2*pi*freq1*time)+sin(2*pi*freq2*time)+sin(2*pi*freq3*time))
+
+# Unfiltered signal - time domain plot
+plt.subplot(2, 1, 1)
+plt.plot(time, signal)
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.title('Unfiltered Signal (Sum of 10 Hz, 35 Hz, 60 Hz Sine Waves) - Time Domain')
+
+plt.show()
