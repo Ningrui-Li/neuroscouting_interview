06ddd2293841218a1657d462c2555d2b9db77768
Ningrui Li <ningrui.li@duke.edu>
2015-01-30 23:20:15 -0500

    changed passband and stopband frequencies from numpy array to python array because it was giving strange results when used as input to buttord

--- matlab_to_python/neuroscouting_exercise_python.py
+++ matlab_to_python/neuroscouting_exercise_python.py
@@ -13,19 +13,20 @@ freq3 = 10
 signal = (1./3)*(sin(2*pi*freq1*time)+sin(2*pi*freq2*time)+sin(2*pi*freq3*time))
 signal_FT = abs(fft(signal))
 
-# create frequency x-axis labels for FT plot
+## create frequency x-axis labels for FT plot
 L = (len(signal)-1)/2
 f = arange(0, sampling_rate/2 + (sampling_rate/2)/L, (sampling_rate/2)/L)
 
-# create 60 Hz notch filter
-Wp = array([58, 62])                # Passband edge frequencies (Hz)
-Ws = array([59, 61])                # Stopband edge frequencies (Hz)
-Wp = Wp / (sampling_rate/2)         # Normalize to radians
-Ws = Ws / (sampling_rate/2)
+## create 60 Hz notch filter
+Wp = [58, 62]                        # Passband edge frequencies (Hz)
+Ws = [59, 61]                        # Stopband edge frequencies (Hz)
+Wp = divide(Wp, sampling_rate/2)     # Normalize to radians
+Ws = divide(Ws, sampling_rate/2)
+
+# Get Butter filter parameters and create bandstop filter using those params
 N, Wn = sig.buttord(Wp, Ws, 3, 40)  # N = order of Butterworth filter
                                     # Wn = Butterworth cutoff frequencies
-print N
-print Wn
+B, A = sig.butter(N, Wn, 'stop')
 
 # limit frequency range between 0 Hz - 75 Hz for FT plot
 frequencyLimitIndices = nonzero(f<75);
@@ -46,5 +47,5 @@ plt.xlabel('Frequency (Hz)')
 plt.ylabel('Power')
 plt.title('Amplitude Spectrum of Unfiltered Signal - Frequency Domain')
 
-#plt.tight_layout()
-#plt.show()
+plt.tight_layout()
+plt.show()
